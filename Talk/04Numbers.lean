80f4ac575a258fe5e99f5301400fc610f429f917
Jannis Limperg <jannis@limperg.de>
2023-09-20 10:05:11 +0200

    Numbers: add positivity, continuity, measurability

--- Talk/04Numbers.lean
+++ Talk/04Numbers.lean
@@ -15,3 +15,20 @@ example (x y z : ℚ) (h1 : 2*x < 3*y) (h2 : -4*x + 2*z < 0)
 
 example (x y : ℤ) : x < 0 → y + x < y := by
   norm_num
+
+example (x : ℤ) (h₁ : 0 ≤ x) (h₂ : x ≠ 0) : 0 < x := by
+  positivity
+
+example (x y : ℤ) (hx : 0 < x) (hy : 0 < y) : 0 < x + y := by
+  positivity
+
+example (x y : ℤ) (hx : 0 ≤ x) (hy : 0 ≤ y) : 0 ≤ x*y := by
+  positivity
+
+example (f g : ℝ → ℝ) (hf : Continuous f) (hg : Continuous g) :
+    Continuous (f ∘ g) := by
+  continuity
+
+example (f g : ℝ → ℝ) (hf : Measurable f) (hg : Measurable g) :
+    Measurable (f ∘ g) := by
+  measurability
