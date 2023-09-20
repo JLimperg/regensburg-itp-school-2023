ea8d6baa1385f9c77c343f1088cd48d0c7a79ebc
Jannis Limperg <jannis@limperg.de>
2023-09-21 00:05:58 +0200

    Logic: remove some material and clarify

--- Talk/01Logic.lean
+++ Talk/01Logic.lean
@@ -1,21 +1,6 @@
 /- # Tactics for Logical Connectives -/
 
-import Mathlib
 import Mathlib.Tactic
-
-/- ## True -/
-
-example : True := by
-  apply True.intro
-
-/- ## False -/
-
-example : False → α := by
-  intro contra
-  cases contra
-
-example : False → α :=
-  False.elim
 
 /- ## Implication, Forall -/
 
@@ -29,68 +14,41 @@ example : ∀ (hab : ∀ (a : A), B) (ha : A), B := by
   apply hab
   exact ha
 
-example : (A → B → C) → A → B → C := by
-  intro habc a b
-  refine habc ?_ b
-  exact a
+/- ## True -/
 
-example : (A → B → C) → A → B → C := by
-  intro habc a b
-  refine' habc _ b
-  exact a
+example : True := by
+  exact True.intro
+
+/- ## False -/
+
+example : False → α := by
+  intro contra
+  cases contra
 
 /- ## And -/
 
 example : A ∧ B → B ∧ A := by
   intro h
-  cases' h with a b
-  constructor
-  · exact b
-  · exact a
-
-example : A ∧ B → B ∧ A := by
-  intro h
-  match h with
-  | ⟨a, b⟩ => constructor <;> assumption
+  cases h with
+  | intro a b =>
+    constructor
+    · assumption
+    · assumption
 
 example : A ∧ B → B ∧ A := by
   intro h
   cases h with
-  | intro a b => constructor <;> assumption
+  | intro a b =>
+    constructor
+    · assumption
+    · assumption
 
 example : A ∧ B → B ∧ A := by
   intro h
-  have ⟨a, b⟩ := h
+  obtain ⟨a, b⟩ := h
   constructor <;> assumption
-
-example : A ∧ B → B ∧ A := by
-  intro ⟨a, b⟩
-  constructor <;> assumption
-
-example : (A ∧ B) ∧ C → A ∧ (B ∧ C) := by
-  intro ⟨x, c⟩
-  have ⟨a, b⟩ := x
-  (repeat' constructor) <;> assumption
-
-example : (A ∧ B) ∧ C → A ∧ (B ∧ C) := by
-  intro ⟨⟨a, b⟩, c⟩
-  (repeat' constructor) <;> assumption
 
 /- ## Or -/
-
-example : A ∨ B → B ∨ A := by
-  intro h
-  cases' h with a b
-  · right
-    exact a
-  · left
-    exact b
-
-example : A ∨ B → B ∨ A := by
-  intro h
-  match h with
-  | .inl a => right; assumption
-  | .inr b => left; assumption
 
 example : A ∨ B → B ∨ A := by
   intro h
@@ -99,27 +57,18 @@ example : A ∨ B → B ∨ A := by
   | inr b => left; assumption
 
 example : A ∨ B → B ∨ A := by
-  rintro (a | b)
-  · right; assumption
-  · left; assumption
-
-example : A ∨ B → B ∨ A := by
   intro h
-  obtain (a | b) := h
-  · right; assumption
-  · left; assumption
-
-example : A ∨ B → B ∨ A := by
-  intro h
-  rcases h with (a | b)
-  · right; assumption
-  · left; assumption
+  cases h
+  case inl a =>
+    right; exact a
+  case inr b =>
+    left; exact b
 
 /- ## Exists, Sigma -/
 
 example {P Q : α → Prop} : (∃ x, P x) → (∀ x, P x → Q x) → ∃ x, Q x := by
   intro ex hPQ
-  cases' ex with x hP
+  obtain ⟨x, hx⟩ := ex
   use x
   aesop
 
@@ -130,7 +79,7 @@ example {P Q : α → Prop} : (∃ x, P x) → (∀ x, P x → Q x) → ∃ x, Q
 
 example {P Q : α → Type} : (Σ x, P x) → (∀ x, P x → Q x) → Σ x, Q x := by
   intro ⟨x, hP⟩ hPQ
-  exists x
+  use x
   aesop
 
 example {P Q : α → Type} : (Σ x, P x) → (∀ x, P x → Q x) → Σ x, Q x := by
