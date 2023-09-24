e2a164b2cbaf64943ffc87a6e6662c60f50de038
Jannis Limperg <jannis@limperg.de>
2023-09-24 14:20:33 +0200

    Help: restructure; add #find, Loogle, mathlib docs

--- Talk/03Help.lean
+++ Talk/03Help.lean
@@ -2,7 +2,26 @@
 
 import Mathlib.Tactic
 
-/- ## Available commands/tactics/... -/
+/- ## Finding Lemmas -/
+
+-- Mathlib docs: https://leanprover-community.github.io/mathlib4_docs/
+
+example {m n : ℕ} : m + n = n + m := by
+  exact?
+
+example {m n : ℕ} : n.succ = m.succ → n = m := by
+  intros; exact?
+
+example {m n : ℕ} : n.succ.succ = m.succ.succ → n = m := by
+  intros
+  fail_if_success exact?
+  apply?
+
+-- #find Real.sin _ = 0
+
+-- Loogle: https://loogle.lean-fro.org
+
+/- ## Finding Available Commands/Tactics/... -/
 
 #help option
 #help tactic
@@ -14,16 +33,3 @@ import Mathlib.Tactic
 #reduce 1 + 1
 -- #eval fun x => x + 1 -- fails
 #reduce fun x => x + 1
-
-/- ## library_search -/
-
-example {m n : ℕ} : m + n = n + m := by
-  exact?
-
-example {m n : ℕ} : n.succ = m.succ → n = m := by
-  exact?
-
-example {m n : ℕ} : n.succ.succ = m.succ.succ → n = m := by
-  intros
-  fail_if_success exact?
-  apply?
