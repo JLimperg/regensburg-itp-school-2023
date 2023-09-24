1e83c2dcbe15a27c8d1243a595a4b875c2b8c665
Jannis Limperg <jannis@limperg.de>
2023-09-24 14:19:15 +0200

    Logic: minor

--- Talk/01Logic.lean
+++ Talk/01Logic.lean
@@ -52,26 +52,29 @@ example : A ∨ B → B ∨ A := by
 example : A ∨ B → B ∨ A := by
   intro h
   cases h
-  case inl a =>
-    right; exact a
   case inr b =>
     left; exact b
+  case inl a =>
+    right; exact a
 
 /- ## Exists, Sigma -/
 
-example {P Q : α → Prop} : (∃ x, P x) → (∀ x, P x → Q x) → ∃ x, Q x := by
+example {P Q : α → Prop} : (∃ x, P x) → (∀ x, P x → Q x) →
+    ∃ x, Q x := by
   intro ex hPQ
   obtain ⟨x, hx⟩ := ex
   use x
   aesop
 
-example {P Q : α → Type} : (Σ x, P x) → (∀ x, P x → Q x) → Σ x, Q x := by
+example {P Q : α → Type} : (Σ x, P x) → (∀ x, P x → Q x) →
+    Σ x, Q x := by
   intro sig hPQ
   obtain ⟨x, hx⟩ := sig
   use x
   aesop
 
-example {P Q : α → Type} : (Σ x, P x) → (∀ x, P x → Q x) → Σ x, Q x := by
+example {P Q : α → Type} : (Σ x, P x) → (∀ x, P x → Q x) →
+    Σ x, Q x := by
   aesop
 
 /- ## Other Basic Tactics -/
@@ -86,10 +89,6 @@ example (n m : ℕ) (h₁ : n < 0) (h₂ : 0 < m) (h₃ : m < 3) : False := by
   clear m h₂ h₃
   have contra := not_lt_zero' h₁
   cases contra
-
-example (a : α) : α := by
-  revert a
-  exact id
 
 example (a : α) : α := by
   revert a
